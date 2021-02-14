a71b6a9b190ac76352e0205fff2db039fcdd5a81
edoardocariati04 <73829089+edoardocariati04@users.noreply.github.com>
2021-02-14 19:13:55 +0100

    initial commit

---
+++ js files /app.js
@@ -0,0 +1,28 @@
+const tl = gsap.timeline({
+    defaults: {ease: "power1.out"}
+});
+
+tl.to('.text', {
+    y:"0%", duration: 1, stagger:0.3
+});
+
+tl.to(".slider", {
+    y:"-100%", duration:1.5, delay: 0.5
+});
+tl.to(
+    ".intro", {y:"-100%", duration: 1}, "-=1"
+    );
+
+tl.fromTo(
+    "nav", { opacity: 0 }, { opacity: 1, duration: 1}
+    );
+tl.fromTo(
+    ".big-text", { opacity: 0 }, { opacity: 1, duration: 1}, "-=1"
+    );
+
+$(".menu").click(function(){
+    $(this).parent().toggleClass("close");
+ });
+
+
+ 
